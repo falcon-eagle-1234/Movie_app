8ded57b1d3e5fb2ac8d71fc394cc0bdf8c0b614b
chinbaa <janchivchinbaa@gmail.com>
2025-02-12 21:03:36 +0800

    genre page

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -5,16 +5,29 @@ import Header from "@/components/Header";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { ChevronRight } from "lucide-react";
+import { useParams } from "next/navigation";
+import { log } from "console";
 
 type genres = {
   name: string;
   id: number;
 };
+type Movie = {
+  id: number;
+  title: string;
+  poster_path: string;
+};
 
 export default function Genres() {
   const [genres, setGenres] = useState<genres[]>([]);
+  const {genreid} = useParams()
+  console.log({genreid});
+  const [genreID, setGenreID] = useState<number[]>([])
+  const [movieGenre, setMovieGenre] = useState<Movie[]>([])
+   
   const movieApiKey = "db430a8098715f8fab36009f57dff9fb";
   const genreList = async () => {
+    // if (genreID.length === 0) return;
     try {
       const response = await fetch(
         `https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${movieApiKey}`
@@ -29,17 +42,52 @@ export default function Genres() {
     }
   };
 
-  console.log(genres);
+
+
+  const genreMovieAll = async () => {
+    if (genreID.length === 0) return;
+    try {
+      const response =  await fetch(`https://api.themoviedb.org/3/discover/movie?language=en&with_genres=${genreID.join(",")}&page=1&api_key=${movieApiKey}`)
+      const result = await response.json();
+      const genreUptate = result.results;
+      setMovieGenre(genreUptate)
+      
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  console.log(movieGenre);
+  
+
+  const handleGenreAdd = (id: number) => {
+    setGenreID((prev) =>
+      prev.includes(id) ? prev.filter((g) => g !== id) : [...prev, id]
+    );
+  };
+
+  console.log(genreID.join(","));
+  console.log(genreID);
+
 
   useEffect(() => {
     genreList();
-  }, []);
-
+    genreMovieAll();
+  }, [genreID]);
+  
+  // useEffect(() => {
+  //   if (genreid) {
+  //     setGenreID([Number(genreid)]);
+  //   }
+  // }, [genreid]);
   return (
     <>
       <Header />
       <div className="flex justify-center">
-        <div className="w-[1280px]">
+
+        <div className="w-[1280px] flex">
           <div className="w-1/3 gap-[32px] flex flex-col">
             <p className="text-[26px] font-bold">Search filter</p>
             <div>
@@ -50,19 +98,36 @@ export default function Genres() {
                 </div>
                 <div className="flex  grid-cols-3 border-r-[2px] border-solid grid">
                   {genres.map((genre, index) => (
-                    <Link key={index} href={`genres/${genre.id}`}>
-                      <p className=" border gap-10px w-fit py-1 px-2 my-[5px] rounded-l-full  rounded-r-full flex items-center gap-1 text-[12px] font-bold">
+                    
+                      <p key={index} onClick={() => handleGenreAdd(genre.id)} className=" border gap-10px w-fit py-1 px-2 my-[5px] rounded-l-full  rounded-r-full flex items-center gap-1 text-[12px] font-bold">
 
                         {genre.name}
                         <ChevronRight size={12} />
                       </p>
-                    </Link>
+                    
                   ))}
                 </div>
               </div>
             </div>
           </div>
-          <div className=" w-2/3"></div>
+          
+          <div className="w-2/3 p-3">
+            <h2 className="text-xl font-bold mb-4">Movies</h2>
+            <div className="grid grid-cols-4 gap-4">
+              {movieGenre.map((movie) => (
+                <Link key={`genredetail${movie.id}`}  href={`/detail/${movie.id}`}>
+                <div key={movie.id} className="text-center">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                    className="w-full h-auto rounded-lg"
+                  />
+                  <p className="text-sm font-medium mt-2">{movie.title}</p>
+                </div>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
--- src/components/ButtonGenre.tsx
+++ src/components/ButtonGenre.tsx
@@ -7,79 +7,88 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { log } from "console";
-import { ChevronDown, ChevronRight} from "lucide-react";
+import { ChevronDown, ChevronRight } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 type genres = {
   name: string;
-  id: string; 
-}
+  id: string;
+};
 
-export  function ButtonDemo() {
-  const [genres, setGenres] = useState<genres[]>([])
+export function ButtonDemo() {
+  const [genres, setGenres] = useState<genres[]>([]);
   const movieApiKey = "db430a8098715f8fab36009f57dff9fb";
   const genreList = async () => {
-   
     try {
       const response = await fetch(
         `https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${movieApiKey}`
       );
-      
+
       const result = await response.json();
-      const genres = result.genres
+      const genres = result.genres;
       console.log(genres);
-      setGenres(genres)
+      setGenres(genres);
     } catch (error) {
       console.log(error);
     }
   };
 
   console.log(genres);
-  const searchParams = useSearchParams()
-  const genreID = searchParams.get("genreid")
-  ? searchParams.get("genreid")?.split(",")
-  : [];
+  const searchParams = useSearchParams();
+  const genreID = searchParams.get("genreid")? searchParams.get("genreid")?.split(",") : [];
   const router = useRouter();
-  const genrePage = (id:string) => {
-    const params = new URLSearchParams(searchParams.toString())
-    genreID?.push(id)
+  const genrePage = (id: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    console.log(params);
+    
+    genreID?.push(id);
     if (genreID) {
       params.set("genreid", genreID?.join(","));
+      console.log(params);
+      console.log(genreID);
+      
+      
     }
-    router.push(`/genres?${params}`)
-  }
+    router.push(`/genres?${params}`);
+  
+    
+  };
+
+  console.log(genreID);
   
 
-  useEffect(()=> {
-    genreList()
-  },[]);
-
+  useEffect(() => {
+    genreList();
+  }, []);
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger >
-      <div className="flex border rounded-md px-4 py-2 text-sm items-center h-9 gap-1">
+      <DropdownMenuTrigger>
+        <div className="flex border rounded-md px-4 py-2 text-sm items-center h-9 gap-1">
           <ChevronDown size={16} />
           <p>Genres</p>
         </div>
       </DropdownMenuTrigger>
       <div className="">
-      <DropdownMenuContent  align="start">
-        <DropdownMenuLabel>
-          <p className="text-2xl">Genres</p>
-          <p className="">See lists of movies by genre</p>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-       <div className="flex  grid-cols-4 grid">
-       {genres.map((genre, index) => (
-        
-        <DropdownMenuItem key={index} onClick={()=>genrePage(genre.id)} className="border rounded-r-full rounded-l-full w-fit p-1 m-1 px-2">{genre.name} <ChevronRight/></DropdownMenuItem>
-      
-          
-        ))}
-       </div>
-      </DropdownMenuContent>
+        <DropdownMenuContent align="start">
+          <DropdownMenuLabel>
+            <p className="text-2xl">Genres</p>
+            <p className="">See lists of movies by genre</p>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <div className="flex  grid-cols-4 grid">
+            {genres.map((genre, index) => (
+              <DropdownMenuItem
+                key={index}
+                onClick={() => genrePage(genre.id)}
+                className="border rounded-r-full rounded-l-full w-fit p-1 m-1 px-2"
+              >
+                {genre.name} <ChevronRight />
+              </DropdownMenuItem>
+            ))}
+          </div>
+        </DropdownMenuContent>
       </div>
     </DropdownMenu>
   );
